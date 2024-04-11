9dc818926a3215461b637d1e4698e0f2a07a2d12
leewoohyuk1 <24robot10709@sonline20.sen.go.kr>
2024-04-11 19:11:21 +0900

    stude

---
+++ js/var.js
@@ -0,0 +1,17 @@
+let name = "이우혁"
+let age = 17
+let height = 165
+let gf = false
+let bf = undefined
+let baby = null
+
+console.log(name)
+console.log(age)
+console.log(height)
+console.log(gf)
+console.log(bf)
+console.log(baby)
+
+// 제이름은 name이고 17살에 키는 165 여친은 f 남자친구는 undefln baby nutll 입니다
+
+console.log("제이름은", name, "이고", age, "살에", "키는", height, "여자친구는", gf,"남자친구는",bf,"애기는",baby, "입니다" )
